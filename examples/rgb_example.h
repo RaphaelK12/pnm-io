6b5bb8f45482175455e3d63cff327172c7f5bd62
Tommy Hinks <tommy.hinks@gmail.com>
2018-10-30 23:21:01 +0100

    added rgb example

---
+++ examples/rgb_example.h
@@ -0,0 +1,17 @@
+// Copyright(C) 2018 Tommy Hinks <tommy.hinks@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#ifndef THINKS_PPM_IO_EXAMPLES_RGB_EXAMPLE_H_INCLUDED
+#define THINKS_PPM_IO_EXAMPLES_RGB_EXAMPLE_H_INCLUDED
+
+#include <string>
+
+
+namespace examples {
+
+void RgbExample(const std::string& filename);
+
+} // namespace examples
+
+#endif // THINKS_PPM_IO_EXAMPLES_RGB_EXAMPLE_H_INCLUDED
